76aab1581523785a2d9128366ed2115ef83c094d
Karine Ferreira Mendes Andrade Silva <824124743@ulife.com.br>
2025-10-06 11:44:32 -0300

    refactor: Refactor gitignore file

---
+++ meu-primeiro-app-react/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client'
+
+const App = () => {
+  return <div style={{margin: 'auto', width: 768, backgroundColor: '#eee', padding: 12, borderRadius: 8}}>
+    <label for="nome" style={{display: 'block', marginBottom: 4}}>Nome:</label>
+    <input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none', boxSizing: 'border-box'}}/>
+    <button style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}}>Enviar</button>
+</div>
+}
+
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
