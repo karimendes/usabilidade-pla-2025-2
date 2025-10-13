774c488109c4d72d096beba7c27256b23cfd091a
Karine Ferreira Mendes Andrade Silva <824124743@ulife.com.br>
2025-10-13 11:05:43 -0300

    update react

--- meu-primeiro-app-react/src/index.js
+++ meu-primeiro-app-react/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
 import {createRoot} from 'react-dom/client'
+import './styles.css'
 
 const App = () => {
+const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: 100, borderRadius: 8}
+
+const textoRotulo = "Nome:"
+
   return <div style={{margin: 'auto', width: 768, backgroundColor: '#eee', padding: 12, borderRadius: 8}}>
-    <label for="nome" style={{display: 'block', marginBottom: 4}}>Nome:</label>
+
+    <label className='rotulo' htmlFor="nome" style={{display: 'block', marginBottom: 4}}>{textoRotulo}</label>
+
     <input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none', boxSizing: 'border-box'}}/>
-    <button style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}}>Enviar</button>
+
+    <button style={estilosBotao}>Enviar</button>
 </div>
 }
 
